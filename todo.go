c5637258672b1fe6a98ca6fda08fe5683fec3b1c
Joe Burns <josebur86@gmail.com>
2018-03-20 21:08:37 -0500

    Clean up command processing.

--- todo.go
+++ todo.go
@@ -8,6 +8,31 @@ import(
 
 var GlobalTodoFile = "W:/todo.md"
 
+type Input struct {
+    Command string
+    Args []string
+}
+func NewInput(args []string) *Input {
+    command := ""
+    commandArgs := []string{}
+    if len(args) > 1 {
+        command = args[1]
+    }
+    if len(args) > 2 {
+        commandArgs = args[2:]
+    }
+
+    return &Input{command, commandArgs}
+}
+
+type command func([]Task, []string) []Task
+type CommandDefinition struct {
+    Name string
+    MinArgCount int
+    Command command
+    RequiresWrite bool
+}
+
 func FindTaskByLine(tasks []Task, line int) *Task {
     for i, _ := range tasks {
         if tasks[i].FileLine == line {
@@ -18,47 +43,55 @@ func FindTaskByLine(tasks []Task, line int) *Task {
     return nil
 }
 
-func main() {
-    args := os.Args
-    argCount := len(args)
+func ListIncompleteTasks(tasks []Task, args []string) []Task {
+    taskCount := 0
+    for _, task := range tasks {
+        if !task.Complete {
+            fmt.Printf("%d %s\n", task.FileLine, task.Description)
+            taskCount++
+        }
+    }
+    fmt.Println("---")
+    fmt.Printf("TODO: %d tasks in %s\n", taskCount, GlobalTodoFile)
 
+    return tasks
+}
+
+func CompleteTask(tasks []Task, args []string) []Task {
+    lineNum, err := strconv.Atoi(args[0])
+    if err != nil {
+        fmt.Print("Invalid line number ", args[0])
+        return tasks
+    }
+
+    task := FindTaskByLine(tasks, lineNum)
+    if task == nil {
+        fmt.Print("No task on line ", lineNum)
+        return tasks
+    }
+
+    task.Complete = true
+
+    return tasks
+}
+
+func main() {
+
+    commands := []CommandDefinition{
+        CommandDefinition{"", 0, ListIncompleteTasks, false},
+        CommandDefinition{"complete", 1, CompleteTask, true},
+    }
+
+    input := NewInput(os.Args)
     tasks := ReadTodoFile(GlobalTodoFile)
 
-    if argCount == 1 {
-        // By default, print out all the uncompleted tasks
-        taskCount := 0
-        for _, task := range tasks {
-            if !task.Complete {
-                fmt.Printf("%d %s\n", task.FileLine, task.Description)
-                taskCount++
-            }
-        }
-        fmt.Println("---")
-        fmt.Printf("TODO: %d tasks in %s\n", taskCount, GlobalTodoFile)
-    } else if argCount > 1 {
-        command := args[1]
-
-        // TODO(joe): this is a mess
-        if command == "complete" {
-            if argCount >= 3 {
-                taskLineNum, err := strconv.Atoi(args[2])
-                if err != nil {
-                    fmt.Println("Invalid line number")
-                } else  {
-                    task := FindTaskByLine(tasks, taskLineNum)
-                    if task != nil {
-                        task.Complete = true
-                        if err := WriteTodos(GlobalTodoFile, tasks); err != nil {
-                            fmt.Print("Unable to write Todos", err)
-                        } else {
-                            fmt.Print("Completed \"", task.Description, "\"")
-                        }
-                    } else {
-                        fmt.Print("No task on line ", taskLineNum)
-                    }
+    for _, commandDef := range commands {
+        if input.Command == commandDef.Name && len(input.Args) >= commandDef.MinArgCount {
+            tasks = commandDef.Command(tasks, input.Args)
+            if commandDef.RequiresWrite {
+                if err := WriteTodos(GlobalTodoFile, tasks); err != nil {
+                    fmt.Print(err)
                 }
-            } else {
-                fmt.Println("todo complete: not enough arguments")
             }
         }
     }
