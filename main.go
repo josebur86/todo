4e11eb7a53f54a8248d0fd2ceb185fafd405e3c0
Burns, Joe <Joe.Burns@elekta.com>
2018-03-27 17:28:37 -0500

    Open tasks are now pulled from a boards In Progress list.

--- main.go
+++ main.go
@@ -12,7 +12,7 @@ func main() {
         os.Exit(1)
     }
 
-    todo.AddCommand(NewCommand("ls", "List all active tasks", ListTasks))
+    //todo.AddCommand(NewCommand("ls", "List all active tasks", ListTasks))
     todo.AddCommand(NewCommand("complete", "Mark the task at the specified line number complete", CompleteTask))
     todo.AddCommand(NewCommand("add", "Adds a task.", AddTask))
     todo.AddCommand(NewCommand("review", "Does a end/beginning of the day review of unfinished tasks.", ReviewTask))
@@ -21,6 +21,7 @@ func main() {
 
     // Trello
     todo.AddCommand(NewCommand("board", "Command for working with boards.", TrelloBoardCommand))
+    todo.AddCommand(NewCommand("ls", "List all active tasks", TrelloListInProgress))
 
     err = todo.Execute()
     if err != nil {
--- trello.go
+++ trello.go
@@ -3,13 +3,29 @@ package main
 import (
     "encoding/json"
     "fmt"
+    "io/ioutil"
     "net/http"
+    "time"
+)
+
+const (
+    BASE_URL = "https://api.trello.com/1"
 )
 
 type Board struct {
     Name string  `json: "name"`
     ID string    `json: "id"`
     Closed bool  `json: "closed"`
+}
+
+type List struct {
+    Name string  `json: "name"`
+    ID string    `json: "id"`
+}
+
+type Card struct {
+    Name string  `json: "name"`
+    ID string    `json: "id"`
 }
 
 func TrelloCommand(t *Todo, command *Command, args []string) error {
@@ -62,8 +78,23 @@ func TrelloBoardCommand(t *Todo, command *Command, args []string) error {
     return nil
 }
 
+func TrelloListInProgress(t *Todo, command *Command, args []string) error {
+    tasks, err := getInProgressTasksFrom(t.State.CurrentBoardID)
+    if err != nil {
+        return err
+    }
+
+    for _, task := range tasks {
+        fmt.Printf("%s\n", task.Description)
+    }
+    fmt.Println("----")
+    fmt.Printf("TODO: %d tasks on board %s.\n", len(tasks), t.State.CurrentBoardName)
+
+    return err
+}
+
 func getOpenBoards() ([]Board, error) {
-    response, err := http.Get(fmt.Sprintf("https://api.trello.com/1/members/%s/boards?key=%s&token=%s", TRELLO_API_USER, TRELLO_API_KEY, TRELLO_API_TOKEN))
+    response, err := http.Get(fmt.Sprintf("%s/members/%s/boards?key=%s&token=%s", BASE_URL, TRELLO_API_USER, TRELLO_API_KEY, TRELLO_API_TOKEN))
     if err != nil {
         return nil, err
     }
@@ -85,4 +116,64 @@ func getOpenBoards() ([]Board, error) {
     }
 
     return boards, nil
+}
+
+func getInProgressTasksFrom(boardID string) ([]Task, error) {
+    response, err := http.Get(fmt.Sprintf("%s/boards/%s/lists?cards=none&key=%s&token=%s", BASE_URL, boardID, TRELLO_API_KEY, TRELLO_API_TOKEN))
+    if err != nil {
+        return nil, err
+    }
+
+    defer response.Body.Close()
+
+    lists := []List{}
+    if err := json.NewDecoder(response.Body).Decode(&lists); err != nil {
+        return nil, err
+    }
+
+    // Find the `In Progress` list
+    for _, list := range lists {
+        if list.Name == "In Progress" {
+            tasks, err := getTasksFrom(list.ID)
+            if err != nil {
+                return nil, err
+            }
+
+            return tasks, nil
+        }
+    }
+
+    return nil, nil
+}
+
+func getTasksFrom(listID string) ([]Task, error) {
+    response, err := http.Get(fmt.Sprintf("%s/lists/%s/cards?key=%s&token=%s", BASE_URL, listID, TRELLO_API_KEY, TRELLO_API_TOKEN))
+    if err != nil {
+        return nil, err
+    }
+
+    defer response.Body.Close()
+
+    cards := []Card{}
+    if err := json.NewDecoder(response.Body).Decode(&cards); err != nil {
+        printResponse(response)
+        return nil, err
+    }
+
+    tasks := []Task{}
+    for _, card := range cards {
+        tasks = append(tasks, Task{0, card.Name, time.Now(), false})
+    }
+
+    return tasks, nil
+}
+
+func printResponse(r *http.Response) {
+    content, err := ioutil.ReadAll(r.Body)
+    if err != nil {
+        fmt.Println(err)
+        return
+    }
+
+    fmt.Println(string(content[:]))
 }
